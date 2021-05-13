d66017f02b5d9285a0c0ce1e7829d15cc837c926
Julien Michel <julienmichel.pro@gmail.com>
2021-05-13 09:54:22 +0200

    [libs] Prepare utils library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/petit-bac', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/petit-bac',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/utils',
+  ],
 };
---
+++ libs/utils/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'utils',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/utils',
+};
